d4d2e1eecaaac9a16c8dbdb1c65ea6ce8c6a9244
Randypaquette13 <Randypaquette13@gmail.com>
2016-02-04 20:06:04 -0500

    Initial Release Lifter

--- src/org/usfirst/frc/team871/robot/Lifter.java
+++ src/org/usfirst/frc/team871/robot/Lifter.java
@@ -1,36 +1,45 @@
 package org.usfirst.frc.team871.robot;
 
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.SpeedController;
 
 public class Lifter {
 
-	SpeedController telescopingMotor;
-	Solenoid liftPiston;
-	DigitalInput grabSense, armUpSense; //TODO: Check if we have these
+	SpeedController telescopingMotor, pullUpMotor; //TODO: Check if there's other motors (winch)
+	
+	Solenoid liftPiston, lockSolenoid;
+	
+	DigitalInput grabSense, armDeployedSense; //TODO: Check if we have these
+	DigitalInput telescopeUpperLimit, telescopeLowerLimit; //TODO: Check if we have these
 	
 	public LifterStates currState = LifterStates.TRANSPORT;
 	
 	boolean autoEnabled = true;
 	
-	public Lifter(SpeedController telescopingMotor, Solenoid raisor, DigitalInput grabSense, DigitalInput armUpSense){
+	public Lifter(SpeedController telescopingMotor, Solenoid raisor, DigitalInput grabSense, DigitalInput armUpSense, Solenoid lockSolenoid, Encoder telescopeEncoder, SpeedController pullUpMotor, DigitalInput armLowerLimit, DigitalInput armUpperLimit){
 		this.telescopingMotor = telescopingMotor;
 		this.liftPiston = raisor;
+		this.lockSolenoid = lockSolenoid;
 		this.grabSense = grabSense;
-		this.armUpSense = armUpSense;
+		this.armDeployedSense = armUpSense;
+		this.pullUpMotor = pullUpMotor;
+		this.telescopeLowerLimit = armLowerLimit;
+		this.telescopeUpperLimit = armUpperLimit;
+		
 	}
 	
 	@SuppressWarnings("unused")
 	public void update(){
 		switch(currState){
-		case ARM_UP:
+		case DEPLOY_ARM:
 			if(Robot.stickJoy.getRisingEdge(Vars.LIFER_ABORT_CONTROL)){
 				currState = LifterStates.TRANSPORT;
 			}
 			
-			if(Robot.stickJoy.getRisingEdge(Vars.LIFER_ADVANCE_CONTROL)){
-				if(armUpSense.get()) currState = LifterStates.TELESCOPE_UP; //TODO: Check if it's active low
+			if(Robot.stickJoy.getRisingEdge(Vars.LIFER_ADVANCE_CONTROL) && armDeployedSense.get()){ //TODO: Check if it's active low
+				currState = LifterStates.EXTEND; 
 			}else{
 				liftPiston.set(true);
 			}
@@ -40,50 +49,72 @@ public class Lifter {
 			if(Robot.stickJoy.getRisingEdge(Vars.LIFER_ABORT_CONTROL)){
 				currState = LifterStates.TRANSPORT;
 			}
-			if(armUpSense.get()){ //TODO: Check if it's active low
-				if(true/*NOT ALL THE WAY UP*/){ //TODO
-					telescopingMotor.set(-1);
+			
+			if(armDeployedSense.get()){ //TODO: Check if it's active low
+				if(!telescopeLowerLimit.get()){ //TODO: Check if it's active low
+					//pullUpMotor.set(-1); //TODO: Check
+					telescopingMotor.set(1);
 				}else{
+					//pullUpMotor.set(0); //TODO: Check
 					telescopingMotor.set(0);
+					
+					currState = LifterStates.LOCKED;
 				}
 			}else{
-				currState = LifterStates.ARM_UP;
+				currState = LifterStates.DEPLOY_ARM;
 			}
 			break;
 			
-		case TELESCOPE_UP:
+		case LOCKED:
+			lockSolenoid.set(true);
+			break;
+			
+		case EXTEND:
 			if(Robot.stickJoy.getRisingEdge(Vars.LIFER_ABORT_CONTROL)){
 				currState = LifterStates.TRANSPORT;
 			}
-			if(armUpSense.get()){ //TODO: Check if it's active low
-				if(true/*NOT FULLY EXTENDED*/){ //TODO
+			
+			if(armDeployedSense.get()){ //TODO: Check if it's active low
+				if(!telescopeUpperLimit.get()){ //TODO: Check if it's active low
 					telescopingMotor.set(1);
+					//pullUpMotor.set(-1); //TODO: Check
 				}else{
-					if(telescopingMotor.get() != 0) telescopingMotor.set(0);
+					telescopingMotor.set(0);
+					//pullUpMotor.set(0); //TODO: Check
+					
 					if(Robot.stickJoy.getRisingEdge(Vars.LIFER_ADVANCE_CONTROL) && !grabSense.get()){ //TODO: Check if it's active low
 						currState = LifterStates.PULL_UP;
 					}
 				}
 			}else{
-				currState = LifterStates.ARM_UP;
+				currState = LifterStates.DEPLOY_ARM;
 			}
 			break;
 			
 		case TRANSPORT:
-			if(true/*ABOVE TRANSPORT*/){ //TODO
-				telescopingMotor.set(-1);
-				if(liftPiston.get()) liftPiston.set(false);
+			if(armDeployedSense.get() || !telescopeLowerLimit.get()){ //TODO: Check if it's active low
+				if(!telescopeLowerLimit.get()){ //TODO: Check if it's active low
+					telescopingMotor.set(-1);
+					//pullUpMotor.set(1); //TODO: Comment this out before booting. (Check Directions) <- VERY IMPORTANT*******************************
+				}
+				else{
+					liftPiston.set(false);
+				}
 			}
-			if(Robot.stickJoy.getRisingEdge(Vars.LIFER_ADVANCE_CONTROL)){
-				currState = LifterStates.ARM_UP;
+			else{
+				if(Robot.stickJoy.getRisingEdge(Vars.LIFER_ADVANCE_CONTROL)){
+					currState = LifterStates.DEPLOY_ARM;
+				}
 			}
 			break;
 			
 		default:
 			//Ur Dumb.
 			break;
-		
 		}
+		
+		telescopingMotor.set(limitedSpeed(telescopingMotor, telescopeUpperLimit, telescopeLowerLimit));
+		pullUpMotor.set(limitedSpeed(pullUpMotor, telescopeUpperLimit, telescopeLowerLimit));
 	}
 	
 	public void doAuto(){ //Spam-Called when auto enabled
@@ -100,11 +131,28 @@ public class Lifter {
 		return autoEnabled;
 	}
 	
+	private double limitedSpeed(SpeedController motor, DigitalInput upperLimit, DigitalInput lowerLimit){
+		double speed = motor.get();
+		
+		if(upperLimit.get() && speed>0){ //TODO: Check if it's active low; Check Directions.
+			speed = 0;
+		}else if(lowerLimit.get() && speed<0){ //TODO: Check if it's active low; Check Directions.
+			speed = 0;
+		}
+		
+		return speed;
+	}
+	
+	public void reset(){
+		currState = LifterStates.TRANSPORT;
+	}
+	
 	public enum LifterStates{
 		TRANSPORT,
-		ARM_UP,
-		TELESCOPE_UP,
-		PULL_UP;
+		DEPLOY_ARM,
+		EXTEND,
+		PULL_UP,
+		LOCKED;
 	}
 	
 }
